cd803c29a31d9b2187da272bddde1e67dc3b386d
Johannes Nau <johannes.nau@outlook.com>
2022-07-04 17:23:13 +0200

    bugfixing

--- deploy/docker-compose/deploy.sh
+++ deploy/docker-compose/deploy.sh
@@ -1,2 +1,2 @@
-scp ./docker-compose.yml $DEPLOY_SERVER:$DEPLOY_DIR/docker-compose.yml
+scp $(dirname "$0")/docker-compose.yml $DEPLOY_SERVER:$DEPLOY_DIR/docker-compose.yml
 ssh $DEPLOY_SERVER "cd $DEPLOY_DIR && docker-compose pull && docker-compose up -d"
