762a1dd6eebaffb5e389c6b806b690468873399b
Johannes Nau <johannes.nau@outlook.com>
2022-07-08 11:02:45 +0200

    bugfixing

--- services/auth/src/config.ts
+++ services/auth/src/config.ts
@@ -8,7 +8,7 @@ function die(reason: string) : string{
 export const config = {
     PORT: parseInt(process.env.PORT ?? "3000"),
     NODE_ENV: process.env.NODE_ENV ?? "development",
-    PASSWORD: process.env.PASSWORD ?? die("the environment variable PASSWORD is not define!"),
+    //PASSWORD: process.env.PASSWORD ?? die("the environment variable PASSWORD is not define!"),
     SECURITY_ISSUER: process.env.SECURITY_ISSUER ?? die("the environment variable SECURITY_ISSUER is not define!"),
     SECURITY_AUDIENCE: process.env.SECURITY_AUDIENCE?? die("the environment variable SECURITY_AUDIENCE is not define!"),
 }
