4b803638b527199d293ad803aea7ea830721a81e
Johannes Nau <johannes.nau@outlook.com>
2022-07-18 16:50:33 +0200

    added goldi specific deployment

---
+++ deploy/docker-compose/deploy_goldi.sh
@@ -0,0 +1,4 @@
+
+envsubst < $(dirname "$0")/docker-compose_goldi.yml > $(dirname "$0")/tmp.yml
+scp $(dirname "$0")/tmp.yml $DEPLOY_SERVER:$DEPLOY_DIR/docker-compose.yml
+ssh $DEPLOY_SERVER "cd $DEPLOY_DIR && docker-compose pull && docker-compose up -d"
