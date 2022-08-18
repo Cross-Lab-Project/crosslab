80219b774bf3559ce3dd13a9151336878eb995f6
Pierre Helbing <pierre.helbing@tu-ilmenau.de>
2022-08-18 19:40:08 +0200

    small change

--- services/auth/src/index.ts
+++ services/auth/src/index.ts
@@ -55,7 +55,7 @@ AppDataSource.initialize()
             JWTVerify: async (jwt, scopes) => {
                 if (!jwt) throw new Error("No jwt found")
                 if (!config.SECURITY_ISSUER) throw new Error("No security issuer specified")
-                const jwksUri = new URL(config.BASE_URL + "/.well-known/jwks.json")
+                const jwksUri = new URL(config.BASE_URL.endsWith("/") ? config.BASE_URL + ".well-known/jwks.json" : config.BASE_URL + "/.well-known/jwks.json")
                 const JWKS = createRemoteJWKSet(jwksUri)
                 const jwtVerifyResult = await jwtVerify(jwt, JWKS, { issuer: config.SECURITY_ISSUER, audience: config.SECURITY_AUDIENCE })
                 const user = jwtVerifyResult.payload as UserType
