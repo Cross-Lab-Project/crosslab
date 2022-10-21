2faf44b41079d66bfc7f94964fd2d0f835051e86
Johannes Nau <johannes.nau@outlook.com>
2022-10-21 09:48:32 +0200

    updates stun/turn server

--- clients/soa/js/src/deviceHandler.ts
+++ clients/soa/js/src/deviceHandler.ts
@@ -61,7 +61,7 @@ export class DeviceHandler {
     if (this.connections.has(message.connectionUrl)) {
       throw Error("Can not create a connection. Connection Id is already present");
     }
-    const connection = new WebRTCPeerConnection({iceServers: [{urls: "stun:stun.l.google.com:19302"}]});
+    const connection = new WebRTCPeerConnection({iceServers: [{urls: "stun:stun.goldi-labs.de:3478"},{urls:"turn:turn.goldi-labs.de:3478"}]});
     connection.tiebreaker = message.tiebreaker;
     this.connections.set(message.connectionUrl, connection);
     for (const serviceConfig of message.services) {
