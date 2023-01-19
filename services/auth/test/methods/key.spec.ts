a9d491aa82e5ddb630fed5c828b285ed751a70e0
Pierre Helbing <pierre.helbing@tu-ilmenau.de>
2023-01-19 12:05:37 +0100

    Add new test file

---
+++ services/auth/test/methods/key.spec.ts
@@ -0,0 +1,9 @@
+export default () => describe("key methods", async function () {
+    describe("generateNewKey()", async function () {
+        // TBD
+    })
+
+    describe("jwk()", async function () {
+        // TBD
+    })
+})
