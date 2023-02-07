a0469b0d06cb75f4bdd49118214a12360d5a7b56
Johannes Nau <johannes.nau@outlook.com>
2023-02-07 20:30:19 +0000

    fix #19

--- helper/common-scripts/node/set-scene.sh
+++ helper/common-scripts/node/set-scene.sh
@@ -6,8 +6,8 @@ ROOT_DIR=$(cd $SCRIPT_DIR/../ && pwd)
 
 cd $ROOT_DIR
 if [ ! -d "$ROOT_DIR/lib" ] && [ ! -d "$ROOT_DIR/app" ]; then
-    tar xf $ROOT_DIR/dist/npm-latest.tgz --strip-components 1 -C $ROOT_DIR --exclude=package.json 2>&1 > /dev/null
+    tar xf $ROOT_DIR/dist/npm-latest.tgz --strip-components 1 -C $ROOT_DIR --exclude=package.json 2>/dev/null >/dev/null
 fi
 if [ ! -d "$ROOT_DIR/node_modules" ]; then
-    npm ci 2>&1 > /dev/null
+    npm ci 2>/dev/null >/dev/null
 fi
