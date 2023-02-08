6fcd0afe6960e4c971a31e9368a5322c75ec1ba8
Johannes Nau <johannes.nau@outlook.com>
2023-02-08 10:00:09 +0000

    update packages

--- scripts/housekeeping/update_package_lock.sh
+++ scripts/housekeeping/update_package_lock.sh
@@ -7,7 +7,7 @@ HELPER=$SCRIPT_DIR/../helper
 npm cache clean --force
 
 for file in $($HELPER/find_files.sh '*/package-lock.json'); do
-  rm -f $file
+  #rm -f $file
   dir=$(dirname $file)
   (cd $dir && npm install) &
 done
