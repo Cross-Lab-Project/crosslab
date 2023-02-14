afdc653d84b39770edaf5e66d486dc68f565012c
Johannes Nau <johannes.nau@outlook.com>
2023-02-14 00:28:37 +0000

    other common files

--- integration-test/.prettierrc.js
+++ integration-test/.prettierrc.js
@@ -1,11 +1,1 @@
-module.exports = {
-    arrowParens: 'avoid',
-    bracketSameLine: true,
-    bracketSpacing: false,
-    singleQuote: true,
-    trailingComma: 'all',
-    printWidth: 140,
-    importOrder: ["^@core/(.*)$", "^@features/(.*)$", "^[./]"],
-    importOrderSeparation: true,
-    importOrderSortSpecifiers: true
-};
+../common/node/.prettierrc.js
