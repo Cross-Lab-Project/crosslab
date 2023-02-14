091167e1558c017216458a11d34736077e6f8d2a
Johannes Nau <johannes.nau@outlook.com>
2023-02-14 17:19:05 +0000

    updated file excludsion in vscode settings

--- scripts/housekeeping.sh
+++ scripts/housekeeping.sh
@@ -6,3 +6,4 @@ WEB_REPOSITORY=https://ci.goldi-labs.de/crosslab
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 $SCRIPT_DIR/housekeeping/generate_readme_badges.sh --repository $REPOSITORY --web-repository $WEB_REPOSITORY $@
 $SCRIPT_DIR/housekeeping/generate_readme_dependencies.sh --repository $REPOSITORY --web-repository $WEB_REPOSITORY $@
+$SCRIPT_DIR/housekeeping/generate_vscode_configs.sh $@
