2ce367a17cd61c00f54eec46cc96994af6dd26bc
Johannes Nau <johannes.nau@outlook.com>
2023-02-20 14:24:53 +0000

    bugfixing version referencing problem

--- common/node/scripts/build.sh
+++ common/node/scripts/build.sh
@@ -29,7 +29,15 @@ npm run build
 mkdir -p dist
 rm -rf dist/*.tgz
 
-cat 'package.json' | jq --arg v "$VERSION" '.version = $v | (.dependencies[] | select(. | startswith("file:")))? = $v | (.devDependencies[] | select(. | startswith("file:")))? = $v | (.peerDependencies[] | select(. | startswith("file:")))? = $v' > package.resolved.json
+dependencies_replacement=$(cat package.json | jq -r '.dependencies + .devDependencies + .peerDependencies | .[]' | grep -E '^file:' | sort | uniq)
+sed_expression=""
+for dependency in $dependencies_replacement; do
+    dependency_path=$(echo "$dependency" | sed 's/file://')
+    archive_path=$(readlink -f $dependency_path/dist/npm-latest.tgz)
+    version=$(tar xfO "$archive_path" package/package.json | jq -r '.version')
+    sed_expression="$sed_expression""s#$dependency#$version#g;"
+done
+cat package.json | sed "$sed_expression" | jq --arg v "$VERSION" '.version = $v' > package.resolved.json
 mv package.json package.bak
 cp package.resolved.json package.json
 file=$(npm pack --pack-destination=./dist/)
