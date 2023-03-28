e3b4527232440da609f781c7c093632aadd1496d
Pierre Helbing <pierre.helbing@tu-ilmenau.de>
2023-03-28 12:24:50 +0200

    Add tests for availability and callback methods

--- services/device/src/methods/callbacks.ts
+++ services/device/src/methods/callbacks.ts
@@ -5,11 +5,13 @@ import { peerconnectionRepository } from '../database/repositories/peerconnectio
 import { deviceUrlFromId, peerconnectionUrlFromId } from './urlFromId'
 import fetch from 'node-fetch'
 
-export const callbackUrl: string =
-    config.BASE_URL + (config.BASE_URL.endsWith('/') ? '' : '/') + 'callbacks/device'
-export const changedCallbacks = new Map<string, Array<string>>()
-export const closedCallbacks = new Map<string, Array<string>>()
-export const statusChangedCallbacks = new Map<string, Array<string>>()
+export const callbackUrl: string = (config.BASE_URL + '/callbacks/device').replace(
+    '//callbacks',
+    '/callbacks'
+)
+export const changedCallbacks = new Map<string, string[] | undefined>()
+export const closedCallbacks = new Map<string, string[] | undefined>()
+export const statusChangedCallbacks = new Map<string, string[] | undefined>()
 
 /**
  * This function sends a 'device-changed' callback.
@@ -35,10 +37,10 @@ export async function sendChangedCallback(device: DeviceModel) {
         })
 
         if (res.status === 410) {
-            const changedCallbackURLs = changedCallbacks.get(device.uuid) ?? []
+            const changedCallbackURLs = changedCallbacks.get(device.uuid)
             changedCallbacks.set(
                 device.uuid,
-                changedCallbackURLs.filter((cb_url) => cb_url != url)
+                changedCallbackURLs?.filter((cb_url) => cb_url != url)
             )
         }
     }
@@ -57,6 +59,7 @@ export async function sendClosedCallback(peerconnection: PeerconnectionModel) {
             )}' to '${url}'`
         )
 
+        // TODO: error handling
         const res = await fetch(url, {
             method: 'post',
             body: JSON.stringify({
@@ -68,10 +71,10 @@ export async function sendClosedCallback(peerconnection: PeerconnectionModel) {
         })
 
         if (res.status === 410) {
-            const closedCallbackURLs = closedCallbacks.get(peerconnection.uuid) ?? []
+            const closedCallbackURLs = closedCallbacks.get(peerconnection.uuid)
             closedCallbacks.set(
                 peerconnection.uuid,
-                closedCallbackURLs.filter((cb_url) => cb_url != url)
+                closedCallbackURLs?.filter((cb_url) => cb_url != url)
             )
         }
     }
@@ -100,11 +103,10 @@ export async function sendStatusChangedCallback(peerconnection: PeerconnectionMo
         })
 
         if (res.status === 410) {
-            const statusCallbackURLs =
-                statusChangedCallbacks.get(peerconnection.uuid) ?? []
+            const statusCallbackURLs = statusChangedCallbacks.get(peerconnection.uuid)
             statusChangedCallbacks.set(
                 peerconnection.uuid,
-                statusCallbackURLs.filter((cb_url) => cb_url != url)
+                statusCallbackURLs?.filter((cb_url) => cb_url != url)
             )
         }
     }
