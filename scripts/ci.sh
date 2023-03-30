de4450181623092c5472ed67d8651552457fec6d
Johannes Nau <johannes.nau@outlook.com>
2023-03-30 10:48:40 +0000

    bugfixing ci

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
-
 REPOSITORY=admin@ci.goldi-labs.de:/data/www/ci/crosslab
 WEB_REPOSITORY=https://ci.goldi-labs.de/crosslab
+
+TAGGED=false
+
+while [[ $# -gt 0 ]]; do
+  key="$1"
+
+  case $key in
+    --tag)
+      TAGGED=true
+      shift # past argument
+      ;;
+
+    *) # unknown option
+      shift # past argument
+    ;;
+  esac
+done
 
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 
 $SCRIPT_DIR/ci.d/ci.sh --repository $REPOSITORY --web-repository $WEB_REPOSITORY $@
-$SCRIPT_DIR/housekeeping.sh --check
+
+if [ "$TAGGED" = false ]; then
+    $SCRIPT_DIR/housekeeping.sh --check
+fi
