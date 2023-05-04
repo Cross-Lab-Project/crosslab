51e1ed914299460e580b717bdad54b5298e11689
Johannes Nau <johannes.nau@outlook.com>
2023-05-04 10:10:19 +0000

    enable upload again

--- scripts/ci.d/upload_artifacts.sh
+++ scripts/ci.d/upload_artifacts.sh
@@ -68,9 +68,6 @@ dist_paths=$( echo "${root[@]}" | tr ' ' $"\n" | LC_ALL=C sort | uniq | sed 's/$
 dist_paths=$(for d in $dist_paths; do [ -d "$d" ] && echo "$d"; done)
 hash_files=$(fd -Ig '*.hash' $dist_paths | sed "s#$GIT_DIR/##")
 
-echo "hash_files: $hash_files"
-exit 0
-
 ref=$(git rev-parse HEAD)
 
 SERVER=${REPOSITORY/:*/}
