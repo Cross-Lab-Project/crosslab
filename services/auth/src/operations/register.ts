f5e37290b788cd52bb372231aeff0b512040ad1f
Pierre Helbing <pierre.helbing@tu-ilmenau.de>
2023-05-04 16:05:26 +0000

    Add registration endpoint

---
+++ services/auth/src/operations/register.ts
@@ -0,0 +1,48 @@
+import { roleRepository } from '../database/repositories/roleRepository'
+import { userRepository } from '../database/repositories/userRepository'
+import { postRegisterSignature } from '../generated/signatures'
+import { RegistrationError } from '../types/errors'
+import { InconsistentDatabaseError, MissingEntityError } from '@crosslab/service-common'
+
+/**
+ * This function implements the functionality for handling POST requests on /register endpoint.
+ * @param body The body of the request.
+ * @throws {AuthenticationError} Thrown if the authentication was unsuccessful.
+ */
+export const postRegister: postRegisterSignature = async (body) => {
+    console.log(`postRegister called`)
+
+    const existingUser = await userRepository.findOne({
+        where: {
+            username: body.username,
+        },
+    })
+
+    if (existingUser)
+        throw new RegistrationError('User with the same username already exists', 400)
+
+    const newUser = await userRepository.create({
+        username: body.username,
+        password: body.password,
+    })
+
+    try {
+        const roleModelUser = await roleRepository.findOneOrFail({
+            where: {
+                name: 'user',
+            },
+        })
+        userRepository.addRoleModelToUserModel(newUser, roleModelUser)
+        await userRepository.save(newUser)
+    } catch (error) {
+        if (error instanceof MissingEntityError)
+            throw new InconsistentDatabaseError("Role 'user' is missing in database", 500)
+        else throw new RegistrationError('User could not be registrated', 500)
+    }
+
+    console.log(`postRegister succeeded`)
+
+    return {
+        status: 201,
+    }
+}
---
+++ services/auth/test/operations/register/post.spec.ts
@@ -0,0 +1,169 @@
+import { roleRepository } from '../../../src/database/repositories/roleRepository'
+import { userRepository } from '../../../src/database/repositories/userRepository'
+import { postLogin } from '../../../src/operations/login'
+import { postRegister } from '../../../src/operations/register'
+import { AuthenticationError, RegistrationError } from '../../../src/types/errors'
+import { TestData } from '../../data/index.spec'
+import { InconsistentDatabaseError } from '@crosslab/service-common'
+import assert from 'assert'
+import Mocha from 'mocha'
+import * as sinon from 'sinon'
+
+export default function (context: Mocha.Context, testData: TestData) {
+    const suite = new Mocha.Suite('POST /register', context)
+
+    let userRepositorySaveStub: sinon.SinonStub<
+        Parameters<typeof userRepository.save>,
+        ReturnType<typeof userRepository.save>
+    >
+    const originalUserRepositorySave = userRepository.save
+
+    suite.beforeEach(function () {
+        userRepositorySaveStub = sinon.stub(userRepository, 'save')
+    })
+
+    suite.afterEach(function () {
+        userRepositorySaveStub.restore()
+    })
+
+    suite.addTest(
+        new Mocha.Test('should register the user successfully', async function () {
+            const username = 'username'
+            const password = 'password'
+
+            userRepositorySaveStub.callsFake(originalUserRepositorySave)
+
+            const result = await postRegister({ username, password })
+
+            assert.strictEqual(result.status, 201)
+
+            const loginResult = await postLogin({ username, password, method: 'local' })
+            assert.strictEqual(loginResult.status, 201)
+            assert(loginResult.body)
+        })
+    )
+
+    suite.addTest(
+        new Mocha.Test(
+            'should throw a RegistrationError if user with the same username already exists',
+            async function () {
+                const username = testData.users.superadmin.model.username
+                const password = 'password'
+
+                userRepositorySaveStub.callsFake(originalUserRepositorySave)
+
+                await assert.rejects(
+                    async () => {
+                        await postRegister({ username, password })
+                    },
+                    (error) => {
+                        assert(error instanceof RegistrationError)
+                        assert.strictEqual(error.status, 400)
+                        assert.strictEqual(
+                            error.message,
+                            'User with the same username already exists'
+                        )
+                        return true
+                    }
+                )
+
+                await assert.rejects(
+                    async () => {
+                        await postLogin({ username, password, method: 'local' })
+                    },
+                    (error) => {
+                        assert(error instanceof AuthenticationError)
+                        assert.strictEqual(error.status, 401)
+                        assert.strictEqual(error.message, 'Invalid login credentials')
+                        return true
+                    }
+                )
+            }
+        )
+    )
+
+    suite.addTest(
+        new Mocha.Test(
+            'should throw a RegistrationError if user cannot be saved',
+            async function () {
+                const username = 'username'
+                const password = 'password'
+                const saveError = new Error('user could not be saved')
+
+                userRepositorySaveStub.throws(saveError)
+
+                await assert.rejects(
+                    async () => {
+                        await postRegister({ username, password })
+                    },
+                    (error) => {
+                        assert(error instanceof RegistrationError)
+                        assert.strictEqual(error.status, 500)
+                        assert.strictEqual(error.message, 'User could not be registrated')
+                        return true
+                    }
+                )
+
+                await assert.rejects(
+                    async () => {
+                        await postLogin({ username, password, method: 'local' })
+                    },
+                    (error) => {
+                        assert(error instanceof AuthenticationError)
+                        assert.strictEqual(error.status, 401)
+                        assert.strictEqual(error.message, 'Invalid login credentials')
+                        return true
+                    }
+                )
+            }
+        )
+    )
+
+    suite.addTest(
+        new Mocha.Test(
+            "should throw an InconsistentDatabaseError if the role 'user' is missing in the database",
+            async function () {
+                const username = 'username'
+                const password = 'password'
+
+                userRepositorySaveStub.callsFake(originalUserRepositorySave)
+
+                const roleModelUser = await roleRepository.findOneOrFail({
+                    where: {
+                        name: 'user',
+                    },
+                })
+                await roleRepository.remove(roleModelUser)
+
+                await assert.rejects(
+                    async () => {
+                        await postRegister({ username, password })
+                    },
+                    (error) => {
+                        assert(error instanceof InconsistentDatabaseError)
+                        assert.strictEqual(error.status, 500)
+                        assert.strictEqual(
+                            error.message,
+                            "Role 'user' is missing in database"
+                        )
+                        return true
+                    }
+                )
+
+                await assert.rejects(
+                    async () => {
+                        await postLogin({ username, password, method: 'local' })
+                    },
+                    (error) => {
+                        assert(error instanceof AuthenticationError)
+                        assert.strictEqual(error.status, 401)
+                        assert.strictEqual(error.message, 'Invalid login credentials')
+                        return true
+                    }
+                )
+            }
+        )
+    )
+
+    return suite
+}
