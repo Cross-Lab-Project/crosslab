79897f1b836bd1f45d947deed14e61b4ad6dc973
Pierre Helbing <pierre.helbing@tu-ilmenau.de>
2023-07-18 15:30:45 +0000

    change test_script
    - move test script
    - move initialization of services from entrypoint to test script

--- services/booking/scripts/test.sh
+++ services/booking/scripts/test.sh
@@ -1,1 +1,33 @@
-../../../common/node/scripts/test.sh
+#!/bin/sh
+
+sudo service rabbitmq-server start
+sudo service mysql start
+sudo mysql -e "CREATE DATABASE unittest DEFAULT CHARSET utf8mb4 DEFAULT COLLATE utf8mb4_unicode_ci;"
+sudo mysql -e "CREATE USER 'test'@localhost IDENTIFIED BY 'test';"
+sudo mysql -e "GRANT ALL PRIVILEGES ON unittest.* to 'test'@localhost;"
+sudo mysql -e "FLUSH PRIVILEGES;"
+
+rm -rf venv
+rm -rf db
+
+npm ci
+
+current=$(pwd)
+result=0
+
+cd "$current/src/schedule-service"
+npm test
+result=$(( $result == 0 ? ( $? == 0 ? 0 : 1 ) : 1 ))
+# cd "$current/src/booking-backend"
+# npm test
+cd "$current/src/device-reservation"
+npm test
+result=$(( $result == 0 ? ( $? == 0 ? 0 : 1 ) : 1 ))
+
+cd $current
+exit $result
+
+sudo mysql -e "DROP USER 'test'@localhost;"
+sudo mysql -e "DROP DATABASE unittest;"
+sudo service mysql stop
+sudo service rabbitmq-server stop
