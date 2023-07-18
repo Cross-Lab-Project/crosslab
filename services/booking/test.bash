e45a77ad025b0226c983045ca4d6184971e70d9d
Pierre Helbing <pierre.helbing@tu-ilmenau.de>
2023-07-18 09:37:33 +0000

    update test-script for booking service

--- services/booking/test.bash
+++ services/booking/test.bash
@@ -5,12 +5,12 @@ result=0
 
 cd "$current/src/schedule-service"
 npm test
-result=$(( $result == 0 ? $? : 1))
+result=$(( $result == 0 ? ( $? == 0 ? 0 : 1 ) : 1 ))
 # cd "$current/src/booking-backend"
 # npm test
 cd "$current/src/device-reservation"
 npm test
-result=$(( $result == 0 ? $? : 1))
+result=$(( $result == 0 ? ( $? == 0 ? 0 : 1 ) : 1 ))
 
 cd $current
 exit $result
