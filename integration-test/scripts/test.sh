f032366d2b6d6f100ba71658d422897c99c7ab08
Johannes Nau <johannes.nau@outlook.com>
2023-08-17 12:50:50 +0000

    fix integration test

--- integration-test/scripts/test.sh
+++ integration-test/scripts/test.sh
@@ -2,7 +2,13 @@
 
 mkdir -p dist
 
-docker-compose build
+export GATEWAY_IMAGE=$(docker load -i ../services/gateway/dist/docker-image.tar | tail -1 | grep -Eo "[^ ]+$")
+export AUTHENTICATION_IMAGE=$(docker load -i ../services/auth/dist/docker-image.tar | tail -1 | grep -Eo "[^ ]+$")
+export AUTHORIZATION_IMAGE=$(docker load -i ../services/authorization/dist/docker-image.tar | tail -1 | grep -Eo "[^ ]+$")
+export DEVICE_IMAGE=$(docker load -i ../services/device/dist/docker-image.tar | tail -1 | grep -Eo "[^ ]+$")
+export EXPERIMENT_IMAGE=$(docker load -i ../services/experiment/dist/docker-image.tar | tail -1 | grep -Eo "[^ ]+$")
+export FEDERATION_IMAGE=$(docker load -i ../services/federation/dist/docker-image.tar | tail -1 | grep -Eo "[^ ]+$")
+
 docker-compose up --no-color > dist/server.log 2>&1 &
 
 rm -rf venv
