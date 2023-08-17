0ef462ba1ab049944c63853a14877bb2b574b3c9
Johannes Nau <johannes.nau@outlook.com>
2023-08-17 11:20:58 +0000

    debug pipeline

--- integration-test/scripts/test.sh
+++ integration-test/scripts/test.sh
@@ -2,19 +2,31 @@
 
 mkdir -p dist
 
-docker-compose build
-docker-compose up --no-color > dist/server.log 2>&1 &
+#docker-compose build
+#docker-compose up --no-color > dist/server.log 2>&1 &
 
-rm -rf venv
-virtualenv venv && venv/bin/pip install -r requirements.txt
-npm ci
+#rm -rf venv
+#virtualenv venv && venv/bin/pip install -r requirements.txt
+#npm ci
+
+end_time=$(($(date +%s) + 60))  # Set end time to 60 seconds from now
 
 for url in "http://localhost/auth/status" "http://localhost/device/status" "http://localhost/auth/status"; do
-    until $(curl --output /dev/null --silent --head --fail $url); do
+    while true; do
+        if curl --output /dev/null --silent --head --fail "$url"; then
+            break  # Exit the loop if the URL is accessible
+        fi
+
+        if [ "$(date +%s)" -ge "$end_time" ]; then
+            echo "Timed out while waiting for the server."
+            exit 1
+        fi
+
         printf '.'
         sleep 5
     done
 done
+
 
 npm run test
 
