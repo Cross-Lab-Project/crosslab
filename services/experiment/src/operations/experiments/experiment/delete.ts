cb4a0f16c8b9970f32982dae2ecdc774eb3a1573
Pierre Helbing <pierre.helbing@tu-ilmenau.de>
2023-11-10 13:53:39 +0000

    add mutex to delete experiment

--- services/experiment/src/operations/experiments/experiment/delete.ts
+++ services/experiment/src/operations/experiments/experiment/delete.ts
@@ -3,6 +3,7 @@ import { logger } from '@crosslab/service-common';
 import { repositories } from '../../../database/dataSource.js';
 import { deleteExperimentsByExperimentIdSignature } from '../../../generated/signatures.js';
 import { finishExperiment } from '../../../methods/experimentStatus/index.js';
+import { mutexManager } from '../../../methods/mutexManager.js';
 import { experimentUrlFromId } from '../../../methods/url.js';
 
 /**
@@ -13,47 +14,57 @@ import { experimentUrlFromId } from '../../../methods/url.js';
  */
 export const deleteExperimentsByExperimentId: deleteExperimentsByExperimentIdSignature =
   async (req, parameters) => {
-    logger.log(
-      'info',
-      `Handling DELETE request on endpoint /experiments/${parameters.experiment_id}`,
+    const release = await mutexManager.acquire(parameters.experiment_id);
+    const createPeerconnectionsRelease = await mutexManager.acquire(
+      `create-peerconnections:${parameters.experiment_id}`,
     );
+    createPeerconnectionsRelease();
 
-    await req.authorization.check_authorization_or_fail(
-      'delete',
-      `experiment:${experimentUrlFromId(parameters.experiment_id)}`,
-    );
+    try {
+      logger.log(
+        'info',
+        `Handling DELETE request on endpoint /experiments/${parameters.experiment_id}`,
+      );
 
-    const experimentModel = await repositories.experiment.findOneOrFail({
-      where: { uuid: parameters.experiment_id },
-      relations: {
-        connections: true,
-        devices: {
-          instance: true,
+      await req.authorization.check_authorization_or_fail(
+        'delete',
+        `experiment:${experimentUrlFromId(parameters.experiment_id)}`,
+      );
+
+      const experimentModel = await repositories.experiment.findOneOrFail({
+        where: { uuid: parameters.experiment_id },
+        relations: {
+          connections: true,
+          devices: {
+            instance: true,
+          },
+          roles: true,
+          serviceConfigurations: {
+            participants: true,
+          },
         },
-        roles: true,
-        serviceConfigurations: {
-          participants: true,
-        },
-      },
-    });
+      });
 
-    if (experimentModel.status !== 'finished')
-      await finishExperiment(experimentModel, req.clients);
+      if (experimentModel.status !== 'finished')
+        await finishExperiment(experimentModel, req.clients);
 
-    await req.authorization.unrelate(
-      req.authorization.user,
-      'owner',
-      `experiment:${experimentUrlFromId(experimentModel.uuid)}`,
-    );
+      await req.authorization.unrelate(
+        req.authorization.user,
+        'owner',
+        `experiment:${experimentUrlFromId(experimentModel.uuid)}`,
+      );
 
-    await repositories.experiment.remove(experimentModel);
+      await repositories.experiment.remove(experimentModel);
 
-    logger.log(
-      'info',
-      `Successfully handled DELETE request on endpoint /experiments/${parameters.experiment_id}`,
-    );
+      logger.log(
+        'info',
+        `Successfully handled DELETE request on endpoint /experiments/${parameters.experiment_id}`,
+      );
 
-    return {
-      status: 204,
-    };
+      return {
+        status: 204,
+      };
+    } finally {
+      release();
+    }
   };
