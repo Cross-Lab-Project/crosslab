ff582ffc09e5b804d736e0d3c1823311e635ad8b
Johannes Nau <johannes.nau@tu-ilmenau.de>
2024-07-24 09:38:54 +0000

    add cookieless authentication

--- services/lti/src/config.ts
+++ services/lti/src/config.ts
@@ -1,5 +1,4 @@
 import { config as CommonConfig } from '@crosslab/service-common';
-import { utils } from '@crosslab/service-common';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -11,7 +10,6 @@ export const config = {
   API_BASE_URL: process.env.API_BASE_URL ?? process.env.BASE_URL ?? 'http://localhost:3009',
   AUTH_SERVICE_URL: process.env.AUTH_SERVICE_URL ?? 'http://localhost:3000',
   EXPERIMENT_SERVICE_URL: process.env.EXPERIMENT_SERVICE_URL ?? 'http://localhost:3002',
-  COOKIE_SECRET: process.env.COOKIE_SECRET ?? utils.die('COOKIE_SECRET not set'),
   orm: {
     ...CommonConfig.readOrmConfig(),
   },
