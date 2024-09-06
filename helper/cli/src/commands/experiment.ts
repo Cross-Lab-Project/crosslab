de0bec7b0c98ea44d4e967f1c1e177e2eb0c8cd0
Johannes Nau <johannes.nau@tu-ilmenau.de>
2024-09-06 13:35:43 +0000

    move to a common CRUD operations

--- helper/cli/src/commands/experiment.ts
+++ helper/cli/src/commands/experiment.ts
@@ -1,77 +1,10 @@
-import { ExperimentServiceTypes } from '@cross-lab-project/api-client';
-import chalk from 'chalk';
+
 import { Command } from 'commander';
-
-import { getClient } from './login.js';
-import { prompt } from './prompt.js';
-
-function shortExperimentList(
-  experiments: ExperimentServiceTypes.ExperimentOverview[],
-  printNumbers: boolean = false,
-) {
-  let ret: string = '';
-  for (const [idx, experiment] of experiments.entries()) {
-    if (printNumbers) {
-      ret += ('[' + idx + ']').padStart(4) + ' ';
-    }
-    ret += chalk.green(experiment.status) + ' ' + chalk.dim(experiment.url) + '\n';
-  }
-  return ret;
-}
-
-async function selecteExperiment(
-  experiments: ExperimentServiceTypes.ExperimentOverview[],
-) {
-  process.stdout.write(shortExperimentList(experiments, true));
-  return experiments[parseInt(await prompt('Select experiment: '))];
-}
+import { CRUD } from './common.js';
 
 export function experiment(program: Command) {
   const experiment = program.command('experiment');
-
-  experiment
-    .command('list')
-    .option('--json', 'Output the JSON response')
-    .action(async options => {
-      const client = await getClient();
-      const experiments = await client.listExperiments();
-      if (options.json) {
-        console.log(JSON.stringify(experiments, null, 2));
-      } else {
-        console.log(experiments);
-      }
-    });
-
-  experiment
-    .command('inspect')
-    .argument('[experiment url]')
-    .action(async (url?: string) => {
-      const client = await getClient();
-      if (url == undefined)
-        url = (await selecteExperiment(await client.listExperiments())).url;
-      if (url == undefined) throw new Error('No device selected');
-      const experiment = await client.getExperiment(url);
-      console.log(experiment);
-    });
-
-  experiment.command('create').action(async () => {
-    const client = await getClient();
-    let experiment = '';
-    for await (const chunk of process.stdin) experiment += chunk;
-
-    const exp = JSON.parse(experiment);
-
-    client.createExperiment(exp);
-  });
-
-  experiment
-    .command('delete')
-    .argument('[experiment url]')
-    .action(async (url?: string) => {
-      const client = await getClient();
-      if (url == undefined)
-        url = (await selecteExperiment(await client.listExperiments())).url;
-      if (url == undefined) throw new Error('No experiment selected');
-      await client.deleteExperiment(url);
-    });
-}
+  
+    CRUD(experiment,{create:'createExperiment', list:'listExperiments', read:'getExperiment', update:'updateExperiment', del:'deleteExperiment'}, 'experiment', (item: any) => ({ name: item.url, url: item.url }));
+  }
+  
--- helper/cli/src/commands/user.ts
+++ helper/cli/src/commands/user.ts
@@ -1,67 +1,9 @@
 import { Command } from 'commander';
 
-import { getClient } from './login.js';
-import { prompt } from './prompt.js';
+import { CRUD } from './common.js';
 
 export function user(program: Command) {
   const user = program.command('user');
 
-  user
-    .command('list')
-    .alias('ls')
-    .option('--json', 'Output the JSON response')
-    .action(async options => {
-      const client = await getClient();
-
-      const users = await client.listUsers();
-
-      if (options.json) {
-        console.log(JSON.stringify(users, null, 2));
-      } else {
-        console.log(users);
-      }
-    });
-
-  user
-    .command('inspect')
-    .argument('[user url]')
-    .action(async (url?: string) => {
-      const client = await getClient();
-
-      if (url == undefined) {
-        console.log('Please provide a user url');
-        return;
-      }
-
-      console.log(await client.getUser(url));
-    });
-
-  user
-    .command('create')
-    .option('-u, --username <username>', 'Username to use for authentication')
-    .option('-p, --password <password>', 'Password to use for authentication')
-    .action(async options => {
-      const client = await getClient();
-      let username: string = options.username;
-      let password: string = options.password;
-
-      if (username == undefined) username = await prompt('Username: ');
-      if (password == undefined) password = await prompt('Password: ', true);
-
-      client.createUser({ username, password });
-    });
-
-  user
-    .command('delete')
-    .argument('[user url]')
-    .action(async (url?: string) => {
-      const client = await getClient();
-
-      if (url == undefined) {
-        console.log('Please provide a user url');
-        return;
-      }
-
-      await client.deleteUser(url);
-    });
+  CRUD(user,{create:'createUser', list:'listUsers', read:'getUser', update:'updateUser', del:'deleteUser'}, 'user', (item: any) => ({ name: item.username, url: item.url }));
 }
