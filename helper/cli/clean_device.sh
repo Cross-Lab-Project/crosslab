a9aa65e8b6002b52f93e9a39a6e56d97728f3e88
Johannes Nau <johannes.nau@tu-ilmenau.de>
2025-02-03 10:35:17 +0100

    refactor: update device and experiment cleanup scripts to use non-JSON output

--- helper/cli/clean_device.sh
+++ helper/cli/clean_device.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-crosslab device list --json | jq '.[] | select ( .name == "ECP" and .type == "device" ) | .url ' | xargs -L 1 -r crosslab device delete
-crosslab device list --json | jq '.[] | select ( .name == "FSM" and .type == "device" ) | .url ' | xargs -L 1 -r crosslab device delete
-crosslab device list --json | jq '.[] | select ( .name == "TI-Lab-ECP" and .type == "device" ) | .url ' | xargs -L 1 -r crosslab device delete
-crosslab device list --json | jq '.[] | select ( .name == "3-Axis-Portal(virtual)" and .type == "device" ) | .url ' | xargs -L 1 -r crosslab device delete
-crosslab device list --json | jq '.[] | select ( .name == "TI - Kombinatorik ECP" and .type == "device" ) | .url ' | xargs -L 1 -r crosslab device delete
+crosslab device list | jq '.[] | select ( .name == "ECP" and .type == "device" ) | .url ' | xargs -L 1 -r crosslab device delete
+crosslab device list | jq '.[] | select ( .name == "FSM" and .type == "device" ) | .url ' | xargs -L 1 -r crosslab device delete
+crosslab device list | jq '.[] | select ( .name == "TI-Lab-ECP" and .type == "device" ) | .url ' | xargs -L 1 -r crosslab device delete
+crosslab device list | jq '.[] | select ( .name == "3-Axis-Portal(virtual)" and .type == "device" ) | .url ' | xargs -L 1 -r crosslab device delete
+crosslab device list | jq '.[] | select ( .description == "Virtual PSPU for the 3 AxisPortal" and .type == "device" ) | .url ' | xargs -L 1 -r crosslab device delete
+crosslab device list | jq '.[] | select ( .name == "TI - Kombinatorik ECP" and .type == "device" ) | .url ' | xargs -L 1 -r crosslab device delete
--- helper/cli/clean_experiments.sh
+++ helper/cli/clean_experiments.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-crosslab experiment list --json | jq '.[] | select ( .status != "finished" ) | .url ' | xargs -L 1 -r crosslab experiment delete
+crosslab experiment list | jq '.[] | select ( .status != "finished" ) | .url ' | xargs -L 1 -r crosslab experiment delete
