e6bdfa5442d54293ea4eb825b0e3864a50f27311
Johannes Nau <johannes.nau@tu-ilmenau.de>
2025-02-03 14:46:49 +0000

    Update dependencies and enhance experiment command functionality

--- helper/cli/scripts/build.sh
+++ helper/cli/scripts/build.sh
@@ -9,13 +9,13 @@ node --experimental-sea-config sea-config.json
 
 # Declare an associative array for binaries
 declare -A binaries=(
-    ["linux-x64"]="https://nodejs.org/dist/v20.18.0/node-v20.18.0-linux-x64.tar.xz"
-    ["linux-armv7l"]="https://nodejs.org/dist/v20.18.0/node-v20.18.0-linux-armv7l.tar.xz"
-    ["linux-arm64"]="https://nodejs.org/dist/v20.18.0/node-v20.18.0-linux-arm64.tar.xz"
-    #["macos-x64"]="https://nodejs.org/dist/v20.18.0/node-v20.18.0-darwin-x64.tar.gz"
-    #["macos-arm64"]="https://nodejs.org/dist/v20.18.0/node-v20.18.0-darwin-arm64.tar.gz"
-    #["windows-x64"]="https://nodejs.org/dist/v20.18.0/node-v20.18.0-win-x64.zip"
-    #["windows-x86"]="https://nodejs.org/dist/v20.18.0/node-v20.18.0-win-x86.zip"
+    ["linux-x64"]="https://nodejs.org/dist/v22.13.1/node-v22.13.1-linux-x64.tar.xz"
+    ["linux-armv7l"]="https://nodejs.org/dist/v22.13.1/node-v22.13.1-linux-armv7l.tar.xz"
+    ["linux-arm64"]="https://nodejs.org/dist/v22.13.1/node-v22.13.1-linux-arm64.tar.xz"
+    ["macos-x64"]="https://nodejs.org/dist/v22.13.1/node-v22.13.1-darwin-x64.tar.gz"
+    ["macos-arm64"]="https://nodejs.org/dist/v22.13.1/node-v22.13.1-darwin-arm64.tar.gz"
+    ["windows-x64"]="https://nodejs.org/dist/v22.13.1/node-v22.13.1-win-x64.zip"
+    ["windows-x86"]="https://nodejs.org/dist/v22.13.1/node-v22.13.1-win-x86.zip"
 )
 
 # Loop through the binaries and download/extract them
@@ -27,20 +27,20 @@ for binary in "${!binaries[@]}"; do
         fi
         tar -xf "build/${binary}.tar.xz" --no-anchored --transform="s/.*/build\/node-${binary}/g" bin/node
         npx postject build/node-${binary} NODE_SEA_BLOB build/index.blob --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2 
-        mv "build/node-${binary}" "dist/crosslab-${binary}"
+        mv "build/node-${binary}" "dist/crosslab-cli-${binary}"
     elif [[ ${binaries[$binary]} == *.tar.gz ]]; then
         if ! [[ -f "build/${binary}.tar.gz" ]]; then
             wget "${binaries[$binary]}" -O "build/${binary}.tar.gz"
         fi
         tar -xf "build/${binary}.tar.gz" --no-anchored --transform="s/.*/build\/node-${binary}/g" bin/node
         npx postject build/node-${binary} NODE_SEA_BLOB build/index.blob --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2 
-        mv "build/node-${binary}" "dist/crosslab-${binary}"
+        mv "build/node-${binary}" "dist/crosslab-cli-${binary}"
     elif [[ ${binaries[$binary]} == *.zip ]]; then
         if ! [[ -f "build/${binary}.zip" ]]; then
             wget "${binaries[$binary]}" -O "build/${binary}.zip"
         fi
         unzip -jp "build/${binary}.zip" */node.exe > "build/node-${binary}.exe"
         npx postject build/node-${binary}.exe NODE_SEA_BLOB build/index.blob --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2 --macho-segment-name NODE_SEA
-        mv "build/node-${binary}.exe" "dist/crosslab-${binary}.exe"
+        mv "build/node-${binary}.exe" "dist/crosslab-cli-${binary}.exe"
     fi
 done
--- helper/cli/src/commands/experiment.ts
+++ helper/cli/src/commands/experiment.ts
@@ -1,10 +1,46 @@
 
+import { APIClient } from '@cross-lab-project/api-client';
 import { Command } from 'commander';
+import open from 'open';
+import { editJson } from '../utils.js';
 import { CRUD } from './common.js';
+import { getClient } from './login.js';
 
 export function experiment(program: Command) {
   const experiment = program.command('experiment');
   
     CRUD(experiment,{create:'createExperiment', list:'listExperiments', read:'getExperiment', update:'updateExperiment', del:'deleteExperiment'}, 'experiment', (item: any) => ({ name: item.url, url: item.url }));
-  }
+
+    experiment
+      .command('run')
+      .argument('[experiment url]')
+      .action(async (url?: string) => {
+        const client = await getClient();
   
+        let obj: any;
+        let response: Awaited<ReturnType<APIClient['createExperiment']>>
+        if (url){
+          obj = await client.getExperiment(url)
+          response = await client.updateExperiment(url, {...obj, status: 'running'})
+        }else{
+          obj = await editJson({});
+          response = await client.createExperiment({...obj, status: 'running'})
+        }
+
+        for (const device of response.instantiatedDevices ?? []) {
+          const url = `${ device.codeUrl }?instanceUrl=${ device.url}&deviceToken=${ device.token }`
+          if (process.stdout.isTTY)
+          {
+              process.stdout.write(`Please open the following url, if the browser does not automatically open:\n  ${url}\n\n`)
+          }
+          open(url)
+        }
+
+        if (process.stdout.isTTY)
+        {
+            process.stdout.write(`Experiment URL: `)
+        }
+        process.stdout.write(response.url)
+        process.stdout.write('\n')
+      });
+  }
