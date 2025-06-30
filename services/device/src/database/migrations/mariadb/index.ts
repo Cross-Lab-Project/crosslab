6bf5a411fa071bab90d875b7ebbda1201f4ef298
Pierre Helbing <pierre.helbing@tu-ilmenau.de>
2025-06-30 11:56:14 +0000

    add device service migration (default additional attributes)

--- services/device/src/database/migrations/mariadb/index.ts
+++ services/device/src/database/migrations/mariadb/index.ts
@@ -4,6 +4,7 @@ import { RemoveAnnouncedAvailability1721035482365 } from './1721035482365-Remove
 import { PeerconnectionConfiguration1721990936450 } from './1721990936450-PeerconnectionConfiguration.js';
 import { AdditionalAttributesDevice1722505432461 } from './1722505432461-AdditionalAttributesDevice.js';
 import { RemoveConnectedProperty1745838329382 } from './1745838329382-RemoveConnectedProperty.js';
+import { DefaultAdditionalAttributes1751017791629 } from './1751017791629-DefaultAdditionalAttributes.js';
 
 export const Migrations = [
   Setup1690370717132,
@@ -12,4 +13,5 @@ export const Migrations = [
   RemoveAnnouncedAvailability1721035482365,
   PeerconnectionConfiguration1721990936450,
   AdditionalAttributesDevice1722505432461,
+  DefaultAdditionalAttributes1751017791629,
 ];
